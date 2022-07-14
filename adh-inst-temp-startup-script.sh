aaf432a119de78b225473716b2f76135407388e0
RangnathKathewad <86666229+RangnathKathewad@users.noreply.github.com>
2022-07-14 10:16:43 +0530

    Create adh-inst-temp-startup-script.sh

---
+++ adh-inst-temp-startup-script.sh
@@ -0,0 +1,14 @@
+#! /bin/bash
+branch=$(curl "http://metadata.google.internal/computeMetadata/v1/instance/attributes/branch" -H "Metadata-Flavor: Google")
+apt update && apt-get install ca-certificates curl gnupg lsb-release make -y
+mkdir -p /etc/apt/keyrings
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | tee /etc/apt/sources.list.d/docker.list > /dev/null
+apt update && apt install docker-ce docker-ce-cli containerd.io -y
+apt install docker-compose-plugin -y
+apt install make -y
+usermod -aG docker ubuntu
+echo "Cloning Beyond compose repo for devops branch"
+token=$(gcloud secrets versions access latest --secret=github-api-tokens)
+su - ubuntu -c "git clone --branch release/production-280622 https://${token}@github.com/med-and-beyond/beyond-compose.git"
+su - ubuntu -c "cd beyond-compose && gcloud auth configure-docker -q && make pull && make up"
